6af0fd7578854c09b85e804c67df949b055b43f6
ltciro <ltciro@gmail.com>
2017-08-25 06:14:02 -0500

    first

---
+++ genstage_example/lib/genstage_example/consumer.ex
@@ -0,0 +1,23 @@
+defmodule GenstageExample.Consumer do
+  use GenStage
+
+  def start_link do
+    GenStage.start_link(__MODULE__, :state_doesnt_matter)
+  end
+
+  def init(state) do
+    {:consumer, state, subscribe_to: [GenstageExample.ProducerConsumer]}
+  end
+
+  def handle_events(events, _from, state) do
+    IO.puts('events #{events}')
+    
+    for event <- events do
+      IO.inspect {self(), event, state}
+    end
+
+    # As a consumer we never emit events
+    {:noreply, [], state}
+  end
+end
+
---
+++ prueba.ex
@@ -0,0 +1,36 @@
+defmodule SimpleQueue do
+  use GenServer
+
+  ### GenServer API
+
+  @doc """
+  GenServer.init/1 callback
+  """
+  def init(state), do: {:ok, state}
+
+  @doc """
+  GenServer.handle_call/3 callback
+  """
+  def handle_call(:dequeue, _from, [value|state]) do
+    {:reply, value, state}
+  end
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
+
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+  ### Client API / Helper functions
+
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+  
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+end
+SimpleQueue.start_link([1, 2, 3])
+
+IO.puts(SimpleQueue.dequeue)
+
+IO.puts(SimpleQueue.dequeue)
+
+IO.puts(SimpleQueue.dequeue)
